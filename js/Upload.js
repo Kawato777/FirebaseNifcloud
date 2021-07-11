b36e4166a5e8de2c6f270546bcea14600e37417f
Kawato <keiaru0512@gmail.com>
2021-07-11 17:40:44 +0900

    CreateSiteStart

--- js/Upload.js
+++ js/Upload.js
@@ -4,11 +4,13 @@ var storageRef = firebase.storage().ref();
 // console.log(firebase.storage().ref());
 var rikoRef = storageRef.child("riko.jpg");
 var fileTarget = document.getElementById("file-data");
+var result = document.getElementById("result");
 
 var btn = document.getElementById("btn");
 btn.addEventListener("click", () => {
     var fileData = fileTarget.files[0];
     rikoRef.put(fileData).then(function(snapshot){
-        console.log("Uploaded a blob or file!");
+        // console.log("Uploaded a blob or file!");
+        result.innerText = "OK!";
     });
 });
