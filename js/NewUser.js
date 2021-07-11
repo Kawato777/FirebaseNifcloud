0b7c9164dff30bc3edf9ad7d4f9e4426646d0972
Kawato <keiaru0512@gmail.com>
2021-07-11 19:54:39 +0900

    userIDpasswordの値参照完了

---
+++ js/NewUser.js
@@ -0,0 +1,17 @@
+// 警告modalセット
+var warningLabel = document.getElementById("warningLabel");
+var warningBody = document.getElementById("warningBody");
+
+function SetWarningModal(label,body) {
+    warningLabel.innerText = label;
+    warningBody.innerText = body;
+}
+
+var userID = document.getElementById("userID");
+var password = document.getElementById("password");
+// userID.value password.value
+
+var createButton = document.getElementById("createButton");
+createButton.addEventListener("click",()=>{
+    
+});
